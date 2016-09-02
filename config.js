2230e6cecd46999d74b1a119cfb96ee478d8e9ae
Nick Parsons <nparsons08@gmail.com>
2016-09-02 14:13:53 -0600

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,5 @@ module.exports = {
     redis: {
         port: 6379,
         host: 'YOUR_REDIS_HOST'
-    },
-    stream: {
-        id: 'YOUR_STREAM_APP_ID',
-        key: 'YOUR_STREAM_APP_KEY',
-        secret: 'YOUR_STREAM_APP_SECRET'
     }
 }
